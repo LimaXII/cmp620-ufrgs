ce4dd3ae0eae0181dde607e30f26324ee56f5a7c
LimaXII <luccasdasilvalima@hotmail.com>
2024-08-19 18:11:20 -0300

    model.py update

--- model.py
+++ model.py
@@ -6,11 +6,13 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Input
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 # Definindo o caminho das pastas
 dataset_path = './dataset'
+
 categories = [
     'Final Fantasy VII Remake', 'Resident Evil 4 Remake', 'Counter Strike 2',
     'Hollow Knight', 'Hogwarts Legacy', 'Baldurs Gate 3', 'Minecraft',
@@ -18,9 +20,10 @@ categories = [
 ]
 
 # Parâmetros
-img_height, img_width = 128, 128
+img_height, img_width = 350, 350
 batch_size = 32
-epochs = 5
+epochs = 10
+num_augmented_images_per_input = 4  # Número de imagens aumentadas por imagem de entrada
 
 # Carregando e pré-processando as imagens
 print("Pré-processando as imagens e fazendo reshape...")
@@ -43,8 +46,41 @@ images, labels = load_images(dataset_path, categories, img_height, img_width)
 images = images / 255.0
 
 print("Pré-processamento concluído. \n")
+
+# Criando o gerador de dados com Data Augmentation
+datagen = ImageDataGenerator(
+    horizontal_flip=True,
+    zoom_range=0.4,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    fill_mode='nearest'
+)
+
+# Gerando imagens aumentadas
+augmented_images = []
+augmented_labels = []
+
+for img, label in zip(images, labels):
+    img = np.expand_dims(img, 0)  # Expande a dimensão para (1, img_height, img_width, 3)
+    i = 0
+    for batch in datagen.flow(img, batch_size=1):
+        augmented_images.append(batch[0])
+        augmented_labels.append(label)
+        i += 1
+        if i >= num_augmented_images_per_input:
+            break
+
+augmented_images = np.array(augmented_images)
+augmented_labels = np.array(augmented_labels)
+
+# Combinando as imagens originais e aumentadas
+X = np.concatenate((images, augmented_images), axis=0)
+y = np.concatenate((labels, augmented_labels), axis=0)
+
 # Dividindo em treino e validação
-X_train, X_val, y_train, y_val = train_test_split(images, labels, test_size=0.2, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Criando o modelo
 model = Sequential([
@@ -70,8 +106,8 @@ history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, vali
 print("Modelo treinado. \n")
 
 # Salvando o modelo
-model.save('./cmp620-ufrgs/models/model_v1.keras')
-print("Modelo salvo em ./models/model_v1.keras")
+model.save('./models/BIG-model-v3.keras')
+print("Modelo salvo em ./models/BIG-model-v3.keras")
 
 # Plotando a acurácia e a perda
 plt.figure(figsize=(12, 4))
